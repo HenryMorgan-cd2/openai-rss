ac1ddee84a6883f383666bca6137e3103e1393a0
Henry Morgan <henry@cd2.uk>
2023-08-09 22:51:49 +0100

    test

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 console.log('Hello world!')
 
 Deno.serve((request) => {
-  return new Response(new TextEncoder().encode('Hello World\n'))
+  return new Response(new TextEncoder().encode('Hello World!!!!\n'))
 })
